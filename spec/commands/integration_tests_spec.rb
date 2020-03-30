289e4f210047b00292cc9362a8995aae1fe8632f
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-03-30 21:21:39 +0100

    Update the integration test trigger tests

--- spec/commands/integration_tests_spec.rb
+++ spec/commands/integration_tests_spec.rb
@@ -4,17 +4,13 @@ describe SlackApplybot::Commands::IntegrationTests, :vcr do
   let(:user_input) { "#{SlackRubyBot.config.user} run tests" }
 
   context 'when user requests a test run' do
-    it 'starts typing' do
+    it 'starts typing and triggers the sidekiq job' do
+      expect(StartIntegrationTestsWorker).to receive(:perform_async)
       expect(message: user_input, channel: 'channel').to start_typing(channel: 'channel')
     end
 
     it 'returns the expected message' do
       expect(message: user_input, channel: 'channel').to be_a Hash
-    end
-
-    xit 'triggers the sidekiq job' do
-      expect { SlackRubyBot::Hooks::Message.new(message: user_input, channel: 'channel') }
-        .to change(MonitorTestRunWorker.jobs, :size).by(1)
     end
   end
 end
