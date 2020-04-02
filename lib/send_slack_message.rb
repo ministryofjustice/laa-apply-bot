586a15935782e012ff0d6b933940341a5926b4d8
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-04-02 09:13:52 +0100

    Replace Config/Settings references

--- lib/send_slack_message.rb
+++ lib/send_slack_message.rb
@@ -1,7 +1,7 @@
 class SendSlackMessage
   def initialize
     Slack.configure do |config|
-      config.token = Settings.slack_api_token
+      config.token = ENV['SLACK_API_TOKEN']
       raise 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
     end
     check_connection
