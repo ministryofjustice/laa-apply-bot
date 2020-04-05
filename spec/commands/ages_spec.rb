2a8ed40179fbae7f82772f1d0a2b5184c8e8df9f
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-04-05 16:00:01 +0100

    Update test by fixing time

--- spec/commands/ages_spec.rb
+++ spec/commands/ages_spec.rb
@@ -7,7 +7,9 @@ describe SlackApplybot::Commands::Ages, :vcr do
     let(:expected_response) { "Apply was deployed 1 day ago\nCFE was deployed 1 day ago" }
 
     it 'starts typing and then returns the expected message' do
-      expect(message: user_input, channel: 'channel').to respond_with_slack_message(expected_response)
+      Timecop.travel(Date.new(2020, 4, 4)) do
+        expect(message: user_input, channel: 'channel').to respond_with_slack_message(expected_response)
+      end
     end
   end
 end
