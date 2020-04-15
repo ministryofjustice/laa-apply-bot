e9e83570441906f8b2464eec5937f51f7b140d63
colinbruce <colin.bruce@digital.justice.gov.uk>
2020-04-15 20:06:50 +0100

    Update ages
    return text english responses for short dates
    e.g. yesterday or today

--- slack-applybot/commands/ages.rb
+++ slack-applybot/commands/ages.rb
@@ -2,20 +2,20 @@ module SlackApplybot
   module Commands
     class Ages < SlackRubyBot::Commands::Base
       command 'ages' do |client, data, _match|
-        apply = Environment.new('apply', 'production')
-        apply_age = (Date.today - Date.parse(apply.ping_data['build_date'])).to_i
-        cfe = Environment.new('cfe', 'production')
-        cfe_age = (Date.today - Date.parse(cfe.ping_data['build_date'])).to_i
+        apply_message = age_message('Apply')
+        cfe_message = age_message('CFE')
         # get master data from github?
-        message_text = "#{age_message('Apply', apply_age)}\n#{age_message('CFE', cfe_age)}"
+        message_text = "#{apply_message}\n#{cfe_message}"
         client.say(channel: data.channel, text: message_text)
       end
 
       class << self
         private
 
-        def age_message(app, age)
-          "#{app} was deployed #{age} #{'day'.pluralize(age)} ago"
+        def age_message(app)
+          application = Environment.new(app.downcase, 'production')
+          deploy_date = Date.parse(application.ping_data['build_date'])
+          "#{app} was deployed #{DateDisplay.call(deploy_date)}"
         end
       end
     end
---
+++ spec/lib/date_display_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'date_display'
+
+describe DateDisplay do
+  subject(:date_display) { described_class.new(date) }
+
+  let(:date) { Date.today }
+
+  it { is_expected.to be_a DateDisplay }
+
+  describe '.call' do
+    subject(:call) { date_display.call }
+
+    context 'when passed today' do
+      it { is_expected.to eq 'today' }
+    end
+
+    context 'when passed yesterday ' do
+      let(:date) { Date.yesterday }
+
+      it { is_expected.to eq 'yesterday' }
+    end
+
+    context 'when passed the day before yesterday ' do
+      let(:date) { Date.today - 2.days }
+
+      it { is_expected.to eq '2 days ago' }
+    end
+  end
+
+  describe '#call' do
+    subject(:call) { described_class.call(date) }
+
+    it { is_expected.to eq 'today' }
+  end
+end
