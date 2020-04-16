e6a6892ed3f4185bd2797a6466033de01559f889
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-04-16 08:17:48 +0100

    make rubocop happy

--- slack-applybot/environment.rb
+++ slack-applybot/environment.rb
@@ -17,26 +17,15 @@ module SlackApplybot
     end
 
     def url
-      @url ||= if NON_LIVE_ENVS.include?(@name)
-                 service_url
-               elsif LIVE_ENV_SYNONYMS.include?(@name)
-                 service_url
-               end
+      @url ||= "#{PREFIX}#{service_url}"
     end
 
     def service_url
-      if @application.downcase.eql?('apply')
-        if NON_LIVE_ENVS.include?(@name)
-          "#{PREFIX}#{@name}.#{SERVICE_URL}"
-        elsif LIVE_ENV_SYNONYMS.include?(@name)
-          "#{PREFIX}#{SERVICE_URL}"
-        end
-      elsif @application.downcase.eql?('cfe')
-        if NON_LIVE_ENVS.include?(@name)
-          "#{PREFIX}check-financial-eligibility-staging.apps.live-1.cloud-platform.service.justice.gov.uk"
-        elsif LIVE_ENV_SYNONYMS.include?(@name)
-          "#{PREFIX}check-financial-eligibility.apps.live-1.cloud-platform.service.justice.gov.uk"
-        end
+      cfe = 'check-financial-eligibility-staging.apps.live-1.cloud-platform.service.justice.gov.uk'
+      if NON_LIVE_ENVS.include?(@name)
+        @application.downcase.eql?('apply') ? "#{@name}.#{SERVICE_URL}" : cfe
+      elsif LIVE_ENV_SYNONYMS.include?(@name)
+        @application.downcase.eql?('apply') ? SERVICE_URL : cfe.gsub('-staging', '')
       end
     end
 
