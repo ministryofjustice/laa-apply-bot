6c6652fa73e563b30c963cc3209ecaca69ef5e50
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-04-25 17:10:02 +0100

    Add uat url command
    Load, reference and test

--- slack-applybot/bot.rb
+++ slack-applybot/bot.rb
@@ -20,6 +20,17 @@ module SlackApplybot
         long_desc 'Starts a remote test run on the linked github repo it will respond to you with a link '\
                   'to the running job on github.  When the job finishes it will message you with the result'
       end
+
+      command 'uat urls' do
+        desc '`@apply-bot uat urls`'
+        long_desc 'Returns a list of all Apply UAT urls currently available'
+      end
+
+      command 'uat url' do
+        desc '`@apply-bot uat url <branch> e.g. @apply-bot uat url ap-999`'
+        long_desc 'This will either show the uat url for the specified branch or, if it cannot be matched,'\
+                  'return an apology and the list of all available uat environments'
+      end
     end
   end
 end
