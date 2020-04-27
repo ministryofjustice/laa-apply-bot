f7a0d24a35896e59376192cf1da837f894b097c2
colinbruce <colin.bruce@digital.justice.gov.uk>
2020-04-27 18:26:19 +0100

    update app.rb
    Add should-matchers setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,9 +43,20 @@ require 'app'
 require 'dotenv'
 Dotenv.load('.env.test')
 
+require 'shoulda/matchers'
 RSpec.configure do |config|
   config.before do
     stub_request(:post, %r{\Ahttps://slack.com/api/.*\z}).to_return(status: 200, body: '', headers: {})
     stub_request(:any, %r{\Ahttps://(www|api).github.com/.*\z}).to_return(status: 200, body: '', headers: {})
+  end
+  config.include(Shoulda::Matchers::ActiveModel, type: :model)
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    # Keep as many of these lines as are necessary:
+    with.library :active_record
   end
 end
