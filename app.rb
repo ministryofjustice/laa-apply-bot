f2d303874d9548055631d2d82493726523d6f38b
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-05-18 14:05:53 +0100

    Update app with 2fa view
    Decodes and displays the qr-code for the current user
    
    Validates the expiry time and secrets before generating
    and displaying

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require 'sidekiq'
 require 'sidekiq/api'
 require 'sidekiq/web'
 require 'dotenv'
+require 'rotp'
+require 'rqrcode'
 require './lib/github_values.rb'
 require './lib/date_display.rb'
 require './lib/kubectl.rb'
@@ -11,16 +13,42 @@ require './lib/start_integration_tests_worker.rb'
 require './lib/monitor_test_run_worker.rb'
 require './lib/slack_attachment.rb'
 require './lib/send_slack_message.rb'
+require './lib/token_generator.rb'
 require './config/sidekiq_config.rb'
 
 dot_file = ENV['ENV'].eql?('test') ? '.env.test' : '.env'
 Dotenv.load(dot_file)
 
 class App < Sinatra::Base
+  set :show_exceptions, :after_handler
+
   get '/' do
     "
     <h1>LAA-Apply bot, find it in slack</h1>
 		<p><a href='/sidekiq'>Dashboard</a></p>
 		"
+  end
+
+  get '/2fa/:token' do |token|
+    begin
+      values = JSON.parse(Base64.urlsafe_decode64(token), object_class: OpenStruct)
+    rescue StandardError
+      return 422
+    end
+    return 403 if Time.now > values.expires_at
+    return 403 unless values.secret.eql?(ENV.fetch('SECRET_KEY_BASE'))
+
+    totp = ROTP::TOTP.new(ROTP::Base32.encode(values.slack_id), issuer: ENV.fetch('SERVICE_NAME'))
+    qrcode = RQRCode::QRCode.new(totp.provisioning_uri(ENV.fetch('SERVICE_EMAIL')))
+
+    "
+    <h1>Your 2FA QR code</h1>
+    <ul>
+    <li>Open google authenticator, or your 2fa app of choice and point it at the code below</li>
+    #{qrcode.as_svg(module_size: 3)}
+    <li>When configured, return to slack and, in a DM with apply-bot, type `2fa-check` followed by
+        the 6 digit code currently in your authenticator app, e.g. `2fa-check 123456`
+    </ul>
+    "
   end
 end
