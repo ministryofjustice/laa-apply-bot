078410d14c95d913af4705b2283961a053ee3a68
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-05-21 09:21:15 +0100

    Add the specific ApplyServices
    For now we just set the values for the name, the base class
    will build the relevant URLs as needed

---
+++ lib/apply_application.rb
@@ -0,0 +1,7 @@
+require 'apply_service/base'
+
+class ApplyApplication < ApplyService::Base
+  def initialize
+    @application = 'Apply'
+  end
+end
---
+++ lib/cfe_application.rb
@@ -0,0 +1,7 @@
+require 'apply_service/base'
+
+class CfeApplication < ApplyService::Base
+  def initialize
+    @application = 'CFE'
+  end
+end
