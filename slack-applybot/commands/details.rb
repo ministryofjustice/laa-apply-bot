b9f3d564a137dc347b52eff074bc17eaa216fbe8
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-06-12 12:31:47 +0100

    Refine the details end point
    replace the Environment call with the new *Instance object

--- slack-applybot/commands/details.rb
+++ slack-applybot/commands/details.rb
@@ -2,14 +2,11 @@ module SlackApplybot
   module Commands
     class Details < SlackRubyBot::Commands::Base
       command 'apply', 'cfe', /details/ do |client, data, match|
-        return unless match['expression'].include?('details')
-
         app = match['command'].downcase
         env = match['expression'].sub('details', '').strip.downcase
         return unless env.split(/,\s|\s/).count.eql?(1)
-        return unless SlackApplybot::Environment.valid?(app, env)
 
-        environment = Environment.new(app, env)
+        environment = "#{app.humanize}Instance".constantize.new(env)
 
         message_text = "`#{environment.name}` details for `#{app}`:```#{environment.ping_data}```"
         client.say(channel: data.channel, text: message_text)
