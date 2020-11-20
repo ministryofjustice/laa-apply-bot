a70fbdc94472fb80809a66fc2cda8ee63f0a5304
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-11-20 11:08:51 +0000

    Update python virtualenv
    Use a python3 one as CCCD was p2 and did not work!

--- bin/build_and_deploy.sh
+++ bin/build_and_deploy.sh
@@ -2,7 +2,7 @@
 cp ~/.kube/config ./config/.kube_config
 docker build -t "754256621582.dkr.ecr.eu-west-2.amazonaws.com/laa-apply-for-legal-aid/laa-apply-bot:app-latest" .
 rm ./config/.kube_config
-workon cccd
+workon p3
 $(aws --profile laa-apply-bot --region eu-west-2 ecr get-login --no-include-email)
 docker push 754256621582.dkr.ecr.eu-west-2.amazonaws.com/laa-apply-for-legal-aid/laa-apply-bot:app-latest
 # TODO: when I can properly be bothered to insert a build tag this won't be needed
