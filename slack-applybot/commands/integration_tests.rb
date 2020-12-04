0153d8498da31a7f0f03de56bd228e580ba9953d
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-12-04 11:42:54 +0000

    Update integration_tests command with channel validity

--- slack-applybot/commands/integration_tests.rb
+++ slack-applybot/commands/integration_tests.rb
@@ -2,8 +2,18 @@ module SlackApplybot
   module Commands
     class IntegrationTests < SlackRubyBot::Commands::Base
       command 'run tests' do |client, data, _match|
-        client.typing(channel: data.channel)
-        TestRunStartWorker.perform_async(data)
+        @client = client
+        @data = data
+        if channel_is_valid?
+          client.typing(channel: data.channel)
+          TestRunStartWorker.perform_async(data)
+        else
+          send_fail
+        end
+      end
+
+      class << self
+        include ChannelValidity
       end
     end
   end
--- spec/commands/integration_tests_spec.rb
+++ spec/commands/integration_tests_spec.rb
@@ -2,6 +2,18 @@ require 'spec_helper'
 
 describe SlackApplybot::Commands::IntegrationTests, :vcr do
   let(:user_input) { "#{SlackRubyBot.config.user} run tests" }
+  before do
+    stub_request(:post, %r{\Ahttps://slack.com/api/conversations.info\z}).to_return(status: 200, body: expected_body)
+  end
+  let(:expected_body) do
+    {
+      'ok': true,
+      'channel': {
+        name: channel
+      }
+    }.to_json
+  end
+  let(:channel) { 'channel' }
 
   context 'when user requests a test run' do
     it 'starts typing and triggers the sidekiq job' do
@@ -10,7 +22,20 @@ describe SlackApplybot::Commands::IntegrationTests, :vcr do
     end
 
     it 'returns the expected message' do
-      expect(message: user_input, channel: 'channel').to be_a Hash
+      expect(message: user_input, channel: channel).to be_a Hash
+    end
+  end
+
+  context 'when the user is not in a channel on the allowed list' do
+    let(:channel) { 'dangerous' }
+    let(:app) { 'simon' }
+    let(:env) { 'staging' }
+    let(:expected_response) { "Sorry <@user>, I don't understand that command!" }
+
+    it 'returns the expected message' do
+      Timecop.travel(Date.new(2020, 4, 16)) do
+        expect(message: user_input, channel: channel).to respond_with_slack_message(expected_response)
+      end
     end
   end
 end
