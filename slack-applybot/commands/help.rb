d036e2b81b88c93a2d0b48ea64548d08f6df4d7a
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-12-11 11:39:57 +0000

    Refactor help
    Only show the add user response when in a non-allowed channel

--- slack-applybot/commands/help.rb
+++ slack-applybot/commands/help.rb
@@ -2,18 +2,34 @@ module SlackRubyBot
   module Commands
     class Help < Base
       command 'help' do |client, data, match|
-        command = match[:expression]
+        @client = client
+        @data = data
+        @command = match[:expression]
 
-        text = if command.present?
-                 Support::Help.instance.command_full_desc(command)
+        text = if channel_is_valid?
+                 text_when_channel_is_public
                else
-                 general_text
+                 text_when_channel_is_shared
                end
         client.say(channel: data.channel, text: text)
       end
 
       class << self
+        include ChannelValidity
+
         private
+
+        def text_when_channel_is_public
+          if @command.present?
+            Support::Help.instance.command_full_desc(@command)
+          else
+            general_text
+          end
+        end
+
+        def text_when_channel_is_shared
+          Support::Help.instance.command_full_desc('add users')
+        end
 
         def general_text
           bot_desc = Support::Help.instance.bot_desc_and_commands
