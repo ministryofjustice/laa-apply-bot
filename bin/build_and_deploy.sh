41007d7949db377e7301540ad6f29f2f10dc6277
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-12-16 07:19:58 +0000

    Update deploy script
    Remove kube config copy in bin script
    It has been replaced by a service account with
    creds in the secrets

--- bin/build_and_deploy.sh
+++ bin/build_and_deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-cp ~/.kube/config ./config/.kube_config
 docker build -t "754256621582.dkr.ecr.eu-west-2.amazonaws.com/laa-apply-for-legal-aid/laa-apply-bot:app-latest" .
-rm ./config/.kube_config
 workon p3
 $(aws --profile laa-apply-bot --region eu-west-2 ecr get-login --no-include-email)
 docker push 754256621582.dkr.ecr.eu-west-2.amazonaws.com/laa-apply-for-legal-aid/laa-apply-bot:app-latest
