d57fb827b99c3133ce981a74c6101450b6b128a1
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-12-16 16:25:50 +0000

    update test
    Make names clearer and improve spacing

--- spec/commands/deploy_reminder_spec.rb
+++ spec/commands/deploy_reminder_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe SlackApplybot::Commands::Ages do
+describe SlackApplybot::Commands::DeployReminder do
   let(:user_input) { 'github-user has a pending production approval for master' }
   before do
     stub_request(:post, %r{\Ahttps://slack.com/api/conversations.info\z}).to_return(status: 200, body: expected_body)
@@ -28,7 +28,8 @@ describe SlackApplybot::Commands::Ages do
   let!(:client) { SlackRubyBot::Client.new }
   let!(:message_command) { SlackRubyBot::Hooks::Message.new }
   let(:params) { Hashie::Mash.new(text: user_input, channel: 'channel', user: 'user') }
-  it 'does-stuff' do
+
+  it 'logs data for analysis' do
     expect(SlackRubyBot::Client.logger).to receive(:warn).with(expected_error)
     message_command.call(client, params)
   end
