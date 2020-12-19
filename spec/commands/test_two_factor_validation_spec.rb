9995e8164a1ddd48054be13b643dc740a0559597
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-12-19 19:46:42 +0000

    Update test_two_factor_validation to use shared example

--- spec/commands/test_two_factor_validation_spec.rb
+++ spec/commands/test_two_factor_validation_spec.rb
@@ -52,18 +52,7 @@ describe SlackApplybot::Commands::TestTwoFactorValidation do
       end
     end
 
-    context 'when the user is not in a channel on the allowed list' do
-      let(:channel) { 'dangerous' }
-      let(:expected_response) { "Sorry <@user>, I don't understand that command!" }
-      let(:expected_hash) do
-        { channel: channel, as_user: true, text: "Sorry <@user>, I don't understand that command!" }
-      end
-
-      it 'raises a message-sending error' do
-        expect_any_instance_of(SendSlackMessage).to receive(:generic).with(expected_hash)
-        expect { message_hook.call(client, params) }.to raise_error ChannelValidity::PublicError
-      end
-    end
+    it_behaves_like 'the channel is invalid'
   end
 
   describe '#2fa-check' do
