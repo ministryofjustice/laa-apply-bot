eac0d0d0f44de415cdde1231ba77b3bbe5145fe0
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-12-19 19:59:23 +0000

    Move uat_url to errors/shared example

--- spec/commands/uat_url_spec.rb
+++ spec/commands/uat_url_spec.rb
@@ -28,14 +28,8 @@ describe SlackApplybot::Commands::UatUrl, :vcr do
     it 'returns the expected message' do
       expect(message: user_input, channel: channel).to respond_with_slack_message(expected_response)
     end
-    context 'when the user is not in a channel on the allowed list' do
-      let(:channel) { 'dangerous' }
-      let(:expected_response) { "Sorry <@user>, I don't understand that command!" }
 
-      it 'returns the expected message' do
-        expect(message: user_input, channel: channel).to respond_with_slack_message(expected_response)
-      end
-    end
+    it_behaves_like 'the channel is invalid'
   end
 
   context 'when user requests a specific branch' do
@@ -66,16 +60,6 @@ describe SlackApplybot::Commands::UatUrl, :vcr do
       end
     end
 
-    context 'when the user is not in a channel on the allowed list' do
-      let(:channel) { 'dangerous' }
-      let(:branch) { 'ap-1234' }
-      let(:expected_response) { "Sorry <@user>, I don't understand that command!" }
-
-      it 'returns the expected message' do
-        Timecop.travel(Date.new(2020, 4, 16)) do
-          expect(message: user_input, channel: channel).to respond_with_slack_message(expected_response)
-        end
-      end
-    end
+    it_behaves_like 'the channel is invalid'
   end
 end
