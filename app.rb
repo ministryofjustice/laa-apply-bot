839042c23750bcfd79dab3236560a5a7f93c71b8
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-12-19 19:24:31 +0000

    Remove legacy qr code handling
    Delete the webpage view and tests

--- app.rb
+++ app.rb
@@ -37,27 +37,4 @@ class App < Sinatra::Base
       build_tag: ENV['BUILD_TAG'] || 'Not Available'
     }.to_json
   end
-
-  get '/2fa/:token' do |token|
-    begin
-      values = JSON.parse(Base64.urlsafe_decode64(token), object_class: OpenStruct)
-    rescue StandardError
-      return 422
-    end
-    return 403 if Time.now > values.expires_at
-    return 403 unless values.secret.eql?(ENV.fetch('SECRET_KEY_BASE'))
-
-    totp = ROTP::TOTP.new(ROTP::Base32.encode(values.slack_id), issuer: ENV.fetch('SERVICE_NAME'))
-    qrcode = RQRCode::QRCode.new(totp.provisioning_uri(ENV.fetch('SERVICE_EMAIL')))
-
-    "
-    <h1>Your 2FA QR code</h1>
-    <ul>
-    <li>Open google authenticator, or your 2fa app of choice and point it at the code below</li>
-    #{qrcode.as_svg(module_size: 3)}
-    <li>When configured, return to slack and, in a DM with apply-bot, type `2fa-check` followed by
-        the 6 digit code currently in your authenticator app, e.g. `2fa-check 123456`
-    </ul>
-    "
-  end
 end
