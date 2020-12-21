df56650d075440d8916339cb422b9e37e53044e0
ColinBruce <colin.bruce@digital.justice.gov.uk>
2020-12-21 08:49:37 +0000

    Update deploy reminder logging and redis-pod

--- slack-applybot/commands/deploy_reminder.rb
+++ slack-applybot/commands/deploy_reminder.rb
@@ -1,14 +1,16 @@
 module SlackApplybot
   module Commands
     class DeployReminder < SlackRubyBot::Commands::Base
+      def self.app_name(match)
+        match[2].present? ? match[2] : 'Apply'
+      end
+
       attachment(/(\S*) has a pending ?(\S*)? production approval for master/) do |_client, data, match|
         result = OpenStruct.new(
           {
             channel: data.channel,
             channel_name: SendSlackMessage.new.conversations_info(channel: data.channel)['channel']['name'],
-            attachment: data.attachments,
-            match: match,
-            proposal: "Find slack id for #{match[0]} and remind them to deploy #{match[1]} to production"
+            proposal: "Find slack id for #{match[1]} and remind them to deploy #{app_name(match)} to production"
           }
         )
         SlackRubyBot::Client.logger.warn(result.to_json)
