3d1679bd02260cdc4cba4362503f43fe3917f92c
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-01-19 14:33:54 +0000

    Add status endpoint
    check whether a git merge commit has merged successfully

---
+++ lib/github/status.rb
@@ -0,0 +1,16 @@
+module Github
+  class Status
+    def initialize(commit_url)
+      @url = "#{commit_url}/status"
+    end
+
+    def call
+      raw_data = RestClient.get(@url, GithubValues.headers)
+      JSON.parse(raw_data)['state']
+    end
+
+    def self.passed?(commit_url)
+      new(commit_url).call.eql?('success')
+    end
+  end
+end
