5f916699ffaa01baf06f3462c8591b48f89c7e78
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-02-15 14:23:06 +0000

    Add Github::PullRequests class
    It uses the github API to pull a list of open PRs

---
+++ spec/lib/github/pull_requests_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+
+RSpec.describe Github::PullRequests do
+  subject(:pull_requests) { described_class.new(application) }
+  let(:application) { ApplyApplication.new }
+  before do
+    stub_request(:any, %r{\Ahttps://(www|api).github.com/.*\z}).to_return(status: 200,
+                                                                          body: truncated_data.to_json,
+                                                                          headers: {})
+  end
+
+  let(:truncated_data) do
+    [
+      { 'head' => { 'ref' => 'ap-1234' } },
+      { 'head' => { 'ref' => 'ap-5432' } }
+    ]
+  end
+
+  describe '.call' do
+    subject(:call) { described_class.call(application) }
+
+    it { is_expected.to eq truncated_data }
+  end
+
+  describe '#call' do
+    subject(:call) { pull_requests.call }
+
+    it { is_expected.to eq truncated_data }
+  end
+end
