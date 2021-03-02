420f19609975dd96ba4a9134f3f48eb1898b0073
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-03-02 14:23:08 +0000

    Implement Helm::Delete function
    Handles the logic of parsing the instruction and displaying errors
    Checks that a dry-run delete works and then deletes

--- slack-applybot/commands/helm.rb
+++ slack-applybot/commands/helm.rb
@@ -1,6 +1,7 @@
 module SlackApplybot
   module Commands
     class Helm < SlackRubyBot::Commands::Base
+      require 'pry-byebug'
       command 'helm' do |client, data, match|
         @client = client
         @data = data
@@ -10,6 +11,8 @@ module SlackApplybot
         message = case match['expression']
                   when 'list', 'tidy'
                     "::Helm::#{match['expression'].capitalize}".constantize.call
+                  when /^delete/
+                    process_delete(match)
                   when nil
                     SlackRubyBot::Commands::Support::Help.instance.command_full_desc('helm')
                   else
@@ -21,7 +24,25 @@ module SlackApplybot
 
       class << self
         include ChannelValidity
+        include TwoFactorAuth
         include UserCommand
+
+        def process_delete(match)
+          parts = match['expression'].split - ['delete']
+          if parts.empty?
+            'Unable to delete - insufficient data, please call as `helm delete name-of-release 000000`'
+          elsif parts.count.eql?(1)
+            'OTP password not provided, please call as `helm delete name-of-release 000000`'
+          elsif validate_otp_part(parts[1])
+            ::Helm::Delete.call(parts[0]) ? "#{parts[0]} deleted" : 'Unable to delete'
+          else
+            'OTP password did not match, please check your authenticator app'
+          end
+        end
+
+        def validate_otp_part(otp)
+          validate(user, otp) if otp.match(/\d*/)
+        end
       end
     end
   end
