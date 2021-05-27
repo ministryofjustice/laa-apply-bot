8bfce527811b9f2e05caf7ac1ede9bdf48f62083
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-05-27 14:21:59 +0100

    Update helm list output

--- lib/helm/list.rb
+++ lib/helm/list.rb
@@ -8,14 +8,30 @@ module Helm
     end
 
     class <<self
+      include GithubBits
+
       private
 
       def header
-        "#{parse_name('Name')}#{parse_state('Status')}Date"
+        # "#{parse_name('Name')}#{parse_state('Status')}#{'Date'.ljust(11)}#{parse_data('Branch')}#{parse_data('PR')}"
+        [
+          parse_name('Name'),
+          parse_state('Status'),
+          'Date'.ljust(11),
+          'PR'.ljust(3, ' '),
+          parse_data('Branch')
+        ].join
       end
 
       def parse_row(data)
-        "#{parse_name(data[0])}#{parse_state(data[1])}#{parse_date(data[2]).strftime('%Y-%m-%d')}"
+        # "#{parse_name(data[0])}#{parse_state(data[1])}#{parse_date(data[2]).strftime('%Y-%m-%d')}"
+        [
+          parse_name(data[0]),
+          parse_state(data[1]),
+          parse_date(data[2]).strftime('%Y-%m-%d').ljust(11),
+          (pr_still_exists(data[0]) ? '✔' : '').ljust(3, ' '),
+          parse_data((branch_still_exists(data[0]) ? '✔' : ''))
+        ].join
       end
 
       def parse_name(name)
@@ -24,6 +40,10 @@ module Helm
 
       def parse_state(state)
         state.ljust(15, ' ')
+      end
+
+      def parse_data(value)
+        value.ljust(7, ' ')
       end
 
       def parse_date(date)
