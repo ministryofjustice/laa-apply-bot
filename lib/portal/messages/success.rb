1b0ae128d76e586fd437477f2b7e01bf0f0cd15d
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-11-16 11:35:13 +0000

    Add Portal::Messages::Success class
    This can be generated for sending to the slack instance

---
+++ lib/portal/messages/success.rb
@@ -0,0 +1,56 @@
+module Portal
+  module Messages
+    class Success
+      def initialize(script)
+        @script = script
+      end
+
+      def self.call(script)
+        new(script).call
+      end
+
+      def call
+        result = []
+        result << block('These user names matched in CCMS')
+        result << block_with_id("```#{@script}```", 'user-script')
+        result << block('Send this script to the new_user channel?')
+        result << action_block('new_user_response') { buttons }
+        result
+      end
+
+      private
+
+      def block(message)
+        { 'type': 'section', 'text': { 'type': 'mrkdwn', 'text': message } }
+      end
+
+      def block_with_id(message, id)
+        { 'block_id': id, 'type': 'section', 'text': { 'type': 'mrkdwn', 'text': message } }
+      end
+
+      def action_block(id)
+        { 'block_id': id, 'type': 'actions', elements: yield }
+      end
+
+      def button(style, text)
+        {
+          'type': 'button',
+          'text': {
+            'type': 'plain_text',
+            'emoji': true,
+            'text': text.capitalize
+          },
+          'style': style,
+          'value': text.downcase
+        }
+      end
+
+      def buttons
+        buttons = []
+        buttons << button('primary', 'approve')
+        buttons << button('danger', 'reject')
+        buttons
+      end
+    end
+  end
+end
---
+++ spec/lib/portal/messages/success_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+RSpec.describe Portal::Messages::Success do
+  subject(:success) { described_class.call(script) }
+  let(:script) { 'contents of script' }
+  let(:expected_response) do
+    [
+      {
+        'type': 'section',
+        'text': {
+          'type': 'mrkdwn',
+          'text': 'These user names matched in CCMS'
+        }
+      },
+      {
+        'block_id': 'user-script',
+        'type': 'section',
+        'text': {
+          'type': 'mrkdwn',
+          'text': '```contents of script```'
+        }
+      },
+      {
+        'type': 'section',
+        'text': {
+          'type': 'mrkdwn',
+          'text': 'Send this script to the new_user channel?'
+        }
+      },
+      {
+        'block_id': 'new_user_response',
+        'type': 'actions',
+        'elements': [
+          {
+            'type': 'button',
+            'text': {
+              'type': 'plain_text',
+              'emoji': true,
+              'text': 'Approve'
+            },
+            'style': 'primary',
+            'value': 'approve'
+          },
+          {
+            'type': 'button',
+            'text': {
+              'type': 'plain_text',
+              'emoji': true,
+              'text': 'Reject'
+            },
+            'style': 'danger',
+            'value': 'reject'
+          }
+        ]
+      }
+    ]
+  end
+
+  it 'returns the expected blocks' do
+    expect(success).to eq expected_response
+  end
+end
