a3efe2744b5127aa305a198052f4399d4a7f7408
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-12-06 09:30:26 +0000

    267: Add an HMRC token

--- scripts/docker-startup.sh
+++ scripts/docker-startup.sh
@@ -3,6 +3,8 @@ echo -en $KUBE_CRT > /tmp/kube.crt
 kubectl config set-cluster live-cluster --certificate-authority=/tmp/kube.crt --server=$KUBE_API_URL
 kubectl config set-credentials apply-user --token=$KUBE_TOKEN_APPLY
 kubectl config set-context apply-context --cluster=live-cluster --user=apply-user --namespace=laa-apply-for-legalaid-uat
+kubectl config set-credentials hmrc-user --token=$KUBE_TOKEN_HMRC
+kubectl config set-context hmrc-context --cluster=live-cluster --user=hmrc-user --namespace=laa-hmrc-interface-uat
 kubectl config use-context apply-context
 
 echo "Migrate Database"
