ca1fff6e753f2b16e8f7d174ae9b4a727953131d
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-12-06 09:29:33 +0000

    267: Update docker-startup
    Rename values to make adding extra contexts more sensible

--- scripts/docker-startup.sh
+++ scripts/docker-startup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo -en $KUBE_CRT > /tmp/kube.crt
-kubectl config set-cluster uat-cluster --certificate-authority=/tmp/kube.crt --server=$KUBE_API_URL
-kubectl config set-context uat-context --cluster=uat-cluster --user=uat-user --namespace=laa-apply-for-legalaid-uat
-kubectl config use-context uat-context
+kubectl config set-cluster live-cluster --certificate-authority=/tmp/kube.crt --server=$KUBE_API_URL
 kubectl config set-credentials apply-user --token=$KUBE_TOKEN_APPLY
+kubectl config set-context apply-context --cluster=live-cluster --user=apply-user --namespace=laa-apply-for-legalaid-uat
+kubectl config use-context apply-context
 
 echo "Migrate Database"
 bundle exec rake db:migrate
