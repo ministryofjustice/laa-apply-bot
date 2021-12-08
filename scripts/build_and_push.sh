5f6789b527ccd7c3f6157fff312d9b5ea49fe7e7
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-12-08 08:49:48 +0000

    Restore apply secrets

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -14,10 +14,7 @@ out=$(docker push ${TAG})
 echo $out
 
 echo "Applying namespace configuration to ${K8S_NAMESPACE}..."
-# TODO: fix the secret.yaml apply, it currently fails on the github action with this error:
-# error parsing kubectl_deploy/secrets.yaml: error converting YAML to JSON: yaml: control characters are not allowed
-# it can be applied correctly locally (new secrets are created but fails from the ubuntu github container
-# kubectl -n ${K8S_NAMESPACE} apply -f kubectl_deploy/secrets.yaml
+kubectl -n ${K8S_NAMESPACE} apply -f kubectl_deploy/secrets.yaml
 kubectl -n ${K8S_NAMESPACE} set image -f kubectl_deploy/website-deployment.yaml website=${TAG} --local -o yaml | kubectl -n ${K8S_NAMESPACE} apply -f -
 kubectl -n ${K8S_NAMESPACE} set image -f kubectl_deploy/sidekiq-deployment.yaml sidekiq=${TAG} --local -o yaml | kubectl -n ${K8S_NAMESPACE} apply -f -
 kubectl -n ${K8S_NAMESPACE} rollout status deployments/website
