5250c59a98e0b1eb06238827be9928575517d62c
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-03-02 16:27:50 +0000

    Address the RSpec/AnyInstance cop for the invalid_channel shared_examples
    Replace allow_any_instance_of with instance_doubles

--- spec/support/shared_examples/invalid_channel.rb
+++ spec/support/shared_examples/invalid_channel.rb
@@ -7,9 +7,16 @@ RSpec.shared_examples "the channel is invalid" do
   let(:expected_hash) do
     { channel:, as_user: true, text: "Sorry <@user>, I don't understand that command!" }
   end
+  let(:ssm) { instance_double("SendSlackMessage") }
+
+  before do
+    allow(SendSlackMessage).to receive(:new).and_return(ssm)
+    allow(ssm).to receive(:generic)
+    allow(ssm).to receive(:conversations_info).and_return(JSON.parse(expected_body))
+  end
 
   it "raises a message-sending error" do
-    expect_any_instance_of(SendSlackMessage).to receive(:generic).with(expected_hash)
+    expect(ssm).to receive(:generic).with(expected_hash)
     expect { message_hook.call(client, params) }.to raise_error ChannelValidity::PublicError
   end
 end
